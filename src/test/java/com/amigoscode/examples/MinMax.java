b8f34415a8eaa665e3f80ecac0abf07b6deb2331
Codensator <86786647+Codensator@users.noreply.github.com>
2024-02-02 13:40:23 +0100

    Update MinMax.java

--- src/test/java/com/amigoscode/examples/MinMax.java
+++ src/test/java/com/amigoscode/examples/MinMax.java
@@ -4,16 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class MinMax {
 
+/** 08.01 - Finding Minimum Number
+ *
+ */
     @Test
     public void min() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        int min = numbers.stream()
+                .min((o1,o2) -> o1.compareTo(o2))
+                .get();
+        System.out.println(min);
     }
 
+/** 08.01 - Finding Maximum Number
+ *
+ */
     @Test
     public void max() {
         List<Integer> numbers = List.of(1, 2, 3, 100, 23, 93, 99);
+        int max = numbers.stream()
+                .max((o1, o2) -> o1.compareTo(o2))
+                .get();
+        System.out.println(max);
     }
 }
