48dc5036b7f8303c8e4c527f9249619d11c051db
Codensator <86786647+Codensator@users.noreply.github.com>
2024-02-04 17:45:37 +0100

    Update DistinctAndSets.java

--- src/test/java/com/amigoscode/examples/DistinctAndSets.java
+++ src/test/java/com/amigoscode/examples/DistinctAndSets.java
@@ -11,13 +11,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DistinctAndSets {
 
+/** 09.01 - Distinct
+ *
+ */
     @Test
     public void distinct() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        List<Integer> distinct = numbers.stream().distinct().collect(Collectors.toList());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
     }
+
+/** 09.02 - Collecting to Set
+ *
+ */
 
     @Test
     public void distinctWithSet() throws Exception {
         List<Integer> numbers = List.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+        Set<Integer> distinct = numbers.stream().collect(Collectors.toSet());
+        assertThat(distinct).hasSize(9);
+        System.out.println(distinct);
     }
 }
